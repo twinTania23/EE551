ec610489426e6d7f7ac8156c18f8884427dc306e
twinTania23 <90424981+twinTania23@users.noreply.github.com>
2021-09-13 14:42:57 -0400

    Initial commit

---
+++ tests/test_numbers_and_strings.py
@@ -0,0 +1,31 @@
+import pytest
+
+from mypkg.my_answers import numbers_and_strings
+
+x, y, z, length, m, n = numbers_and_strings()
+
+
+def test_power():
+    assert(x == 1024)
+
+
+def test_string():
+    assert(y == "Stevens")
+
+
+def test_repeat():
+    assert(len(z) == 7 * len("Stevens"))
+
+
+def test_len():
+    assert(length == 49)
+
+
+def test_concat():
+    assert(m == "Stevens is Great")
+
+
+def test_replace():
+    assert(n == "Stevens is Good")
+
+
