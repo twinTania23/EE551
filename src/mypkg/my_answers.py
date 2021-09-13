c3656d30d74472e98dd7161c83a9e8cdc2326dd0
twinTania23 <90424981+twinTania23@users.noreply.github.com>
2021-09-13 18:21:58 -0400

    Update my_answers.py

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -40,20 +40,22 @@ def lists():
     p = ['Hoboken','is','awesome']
 
     # Get all the items past the first of the third substring
-    r = 
+    r = list(p[2])[1:]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    A = 
+    A = [[1,4,5],
+         [6,10,11],
+         [12,17,38]]
 
     # Collect the items in the last column of matrix A using list comprehension
-    c = 
+    c = [row[-1]for row in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-    d = 
-
+    d = [A[i][i]for i in [0,1,2]]
+    d = [row for row in d if row%2==0]
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
